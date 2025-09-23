bc2318e22167b6d02c6f5150c3f22e943204a307
LUIS DANIEL GONZALEZ CORREA <luisdaniel.gonzalez@udea.edu.co>
2025-09-23 14:12:45 -0500

    Agrego funciones inversas de XOR
    Signed-off-by: LUIS DANIEL GONZALEZ CORREA <luisdaniel.gonzalez@udea.edu.co>

--- Codigo/desafio1/main.cpp
+++ Codigo/desafio1/main.cpp
@@ -2,6 +2,35 @@
 
 using namespace std;
 
+
+void aplicarXOR(unsigned char* buffer, int size, unsigned char K) // XOR
+{
+    for (int i = 0; i < size; i++) {
+        buffer[i] = buffer[i] ^ K;
+    }
+}
+
+
+unsigned char rotarDerecha(unsigned char byte, int n) // Rotacion derecha byte
+{
+
+    for (int i = 0; i < n; i++) {
+        unsigned char ultimoBit = byte % 2;
+        byte = byte / 2;
+        if (ultimoBit == 1) {
+            byte = byte + 128;
+        }
+    }
+    return byte;
+}
+
+void aplicarRotacion(unsigned char* buffer, int size, int n) // Aplica rotación dereca a buffer
+{
+    for (int i = 0; i < size; i++) {
+        buffer[i] = rotarDerecha(buffer[i], n);
+    }
+}
+
 int main()
 {
     cout << "Desafio 1 - Informatica 2. Descomprimo y desencripto." << endl;
